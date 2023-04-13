71c81dcd9f927efe4922425aef65d86372ac691b
Remo <81964236+remoremote@users.noreply.github.com>
2023-04-13 13:44:16 +0200

    create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("article-form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const companyName = document.getElementById("company-name").value;
+  const companyWebsite = document.getElementById("company-website").value;
+  const topic = document.getElementById("topic").value;
+  const article = document.getElementById("article").value;
+
+  const response = await fetch("/submit-article", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ companyName, companyWebsite, topic, article }),
+  });
+
+  if (response.ok) {
+    alert("Article submitted successfully!");
+  } else {
+    alert("An error occurred. Please try again.");
+  }
+});
